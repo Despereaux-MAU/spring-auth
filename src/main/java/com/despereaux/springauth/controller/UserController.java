9df81a837c9802bdf445222cf7f14b8b36911411
Despereaux <atousa0507@gmail.com>
2024-10-10 16:22:34 +0900

    로그인 구현 : JWT

--- src/main/java/com/despereaux/springauth/controller/UserController.java
+++ src/main/java/com/despereaux/springauth/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.despereaux.springauth.controller;
 
+import com.despereaux.springauth.dto.LoginRequestDto;
 import com.despereaux.springauth.dto.SignupRequestDto;
 import com.despereaux.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,5 +33,16 @@ public class UserController {
     public String signup(SignupRequestDto requestDto) { // @model Attribute 생략 가능
         userService.signup(requestDto);
         return "redirect:/api/user/login-page";
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) { // @model Attribute 생략 가능
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error"; // ?error 추가
+        }
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/despereaux/springauth/dto/LoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.despereaux.springauth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
