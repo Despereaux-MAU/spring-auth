8310481df80b04c8ecc60c11c2cf2113096bf375
Despereaux <rlaehdwn025@naver.com>
2024-10-13 10:45:23 +0900

    접근 불가 페이지

--- src/main/java/com/despereaux/springauth/controller/ProductController.java
+++ src/main/java/com/despereaux/springauth/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.despereaux.springauth.controller;
 
 import com.despereaux.springauth.entity.User;
+import com.despereaux.springauth.entity.UserRoleEnum;
 import com.despereaux.springauth.security.UserDetailsImpl;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,17 @@ public class ProductController {
         User user = userDetails.getUser();
         System.out.println("user.getUsername() = " + user.getUsername());
         System.out.println("user.getEmail() = " + user.getEmail());
+
+        return "redirect:/";
+    }
+
+    @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용
+    @GetMapping("/products/secured")
+    public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
 
         return "redirect:/";
     }
