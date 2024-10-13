af6e51963270d4dede3eb343a648fe9a1016ea80
Despereaux <rlaehdwn025@naver.com>
2024-10-13 11:11:02 +0900

    Validation(데이터 검증하기)

--- src/main/java/com/despereaux/springauth/controller/ProductController.java
+++ src/main/java/com/despereaux/springauth/controller/ProductController.java
@@ -1,14 +1,15 @@
 package com.despereaux.springauth.controller;
 
+import com.despereaux.springauth.dto.ProductRequestDto;
 import com.despereaux.springauth.entity.User;
 import com.despereaux.springauth.entity.UserRoleEnum;
 import com.despereaux.springauth.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api")
@@ -34,5 +35,11 @@ public class ProductController {
         }
 
         return "redirect:/";
+    }
+
+    @PostMapping("/validation")
+    @ResponseBody
+    public ProductRequestDto testValid(@RequestBody @Valid ProductRequestDto requestDto) {
+        return requestDto;
     }
 }
